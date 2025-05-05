63728f62d80e3d29038980ebbb1590d88a354277
Douglas <ftsgmr@gmail.com>
2025-05-05 18:52:04 -0300

    add-js

---
+++ script.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('form');
+const newButton = document.getElementById('button');
+const newName = document.getElementById('nome');
+const newPassword = document.getElementById('senha');
+const confirmPassword = document.getElementById('confirmarSenha');
+const newText = document.getElementById('text');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+   
+
+    if(newPassword.value !== confirmPassword.value) {
+        alert('As senhas não coincidem!');
+    } else {
+        newText.textContent = 'Cadastro realizado com sucesso!';
+        form.reset();
+    }
+    
+})
+
+
